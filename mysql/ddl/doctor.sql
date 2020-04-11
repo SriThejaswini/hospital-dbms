ac3d30b5b84704b9208cacbd569036e1a61a410e
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-11 19:43:18 -0400

    updated doctor.sql

--- mysql/ddl/doctor.sql
+++ mysql/ddl/doctor.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS doctor;
 
 CREATE TABLE IF NOT EXISTS doctor (
-  doctor_id int NOT NULL PRIMARY KEY,
-  doctor_name varchar(255) default NULL,
-  department_id int references department(department_id),
+  doctor_id int NOT NULL primary key,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  department_id int NOT NULL references department(department_id),
   specialization_id int default NULL references specialization(specialization_id),
   contact_number varchar(100) default NULL
 );
