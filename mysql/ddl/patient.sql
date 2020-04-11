cae43f88103e3e4eb4177a4715aea82a0c22ed88
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-11 19:53:41 -0400

    Updated patient.sql

--- mysql/ddl/patient.sql
+++ mysql/ddl/patient.sql
@@ -1,13 +1,12 @@
 DROP TABLE IF EXISTS patient;
 
 CREATE TABLE IF NOT EXISTS patient (
-  patient_id int NOT NULL auto_increment,
-  name varchar(255) default NULL,
+  patient_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
   gender varchar(255) default NULL,
-  current_condition varchar(255) default NULL,
   blood_group varchar(255) default NULL,
+  current_condition varchar(255) default NULL,
   insurance_covered varchar(255) default NULL,
-  insurance_name varchar(255) default NULL,
-  primary key(patient_id)
-) AUTO_INCREMENT=1;
-
+  insurance_name varchar(255) default NULL
+)AUTO_INCREMENT = 1;
