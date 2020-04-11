eab103394f11bd93d186786d7ab365a3f6e3e4d1
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-11 18:17:12 -0400

    Create table statements

---
+++ mysql/ddl_create_tables.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS patient;
+
+CREATE TABLE patient (
+  patient_id int NOT NULL auto_increment,
+  name varchar(255) default NULL,
+  gender varchar(255) default NULL,
+  current_condition varchar(255) default NULL,
+  blood_group varchar(255) default NULL,
+  insurance_covered varchar(255) default NULL,
+  insurance_name varchar(255) default NULL,
+  primary key(patient_id)
+) AUTO_INCREMENT=1;
+
