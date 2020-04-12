3cb6e3ec50d896ad6f739453a8b59ded9b76a37d
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-12 01:16:28 -0400

    create 08_insert_Purchases.sql

---
+++ mysql/dml/08_insert_Purchases.sql
@@ -0,0 +1,10 @@
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (80,34),(86,74),(28,88),(26,9),(35,50),(38,41),(10,36),(82,87),(41,66),(92,91);
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (32,27),(76,64),(47,69),(68,91),(22,18),(33,45),(22,25),(51,60),(11,78),(42,67);
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (91,9),(92,60),(81,90),(77,54),(77,7),(63,36),(13,62),(29,19),(74,7),(8,56);
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (11,18),(20,17),(71,93),(87,83),(58,82),(57,95),(34,58),(18,4),(45,65),(16,17);
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (83,23),(75,98),(46,81),(59,54),(64,96),(13,90),(88,56),(42,16),(65,43),(1,35);
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (63,7),(17,35),(83,56),(54,73),(18,63),(80,83),(35,96),(25,71),(86,9),(24,6);
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (50,83),(75,15),(66,84),(48,11),(27,76),(88,38),(69,61),(35,78),(8,22),(77,25);
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (98,54),(27,66),(9,81),(15,13),(78,32),(1,47),(55,80),(33,36),(75,33),(28,65);
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (66,55),(79,47),(30,28),(33,57),(93,60),(59,70),(3,10),(24,63),(91,72),(91,59);
+INSERT INTO purchases (pharmacy_id,patient_id) VALUES (75,17),(79,9),(29,90),(73,27),(13,87),(34,85),(83,37),(60,64),(21,62),(98,60);
