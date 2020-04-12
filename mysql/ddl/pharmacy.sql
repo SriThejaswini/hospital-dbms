39abd58cba9d1fe71eda282cbf87e852ced7c403
AakashB-1396 <58054718+AakashB-1396@users.noreply.github.com>
2020-04-11 21:51:40 -0400

    Update pharmacy.sql

--- mysql/ddl/pharmacy.sql
+++ mysql/ddl/pharmacy.sql
@@ -1,15 +1,14 @@
 Create SCHEMA Project2;
 Create database Project;
 use Project;
+DROP TABLE IF EXISTS pharmacy;
 
-Drop TABLE Pharmacy;
-CREATE TABLE `Pharmacy` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `PharmacyName` varchar(255),
-  `Email` varchar(255) default NULL,
-  `Address` varchar(255) default NULL,
-  `City` varchar(255),
-  `Phone` varchar(11) default NULL,
-  `Fax` varchar(100) default NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS pharmacy (
+  pharmacy_id int NOT NULL PRIMARY KEY auto_increment,
+  pharmacy_name varchar(255),
+  email varchar(255) default NULL,
+  address varchar(255) default NULL,
+  zip varchar(10) default NULL,
+  phone varchar(11) default NULL,
+  fax varchar(100) default NULL
 ) AUTO_INCREMENT=1;
