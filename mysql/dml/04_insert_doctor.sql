5d490165220d9d8f7fbac5768218500d003b9c2f
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-12 00:38:01 -0400

    Updated department-specialization details

--- mysql/dml/04_insert_doctor.sql
+++ mysql/dml/04_insert_doctor.sql
@@ -1,2 +1,21 @@
-INSERT INTO doctor (doctor_id,first_name,last_name,department_id,specialization_id,contact_number) VALUES (1,"Garth","Walter",3,4,"1-667-914-4477"),(2,"Regan","Alvarado",4,3,"1-307-269-8198"),(3,"Hiroko","Ortega",1,3,"1-453-403-4247"),(4,"Bertha","Woodward",7,4,"1-495-522-6824"),(5,"Britanney","Maynard",10,2,"1-634-229-3941"),(6,"Kasper","Levine",1,3,"1-139-291-3738"),(7,"Orlando","Carr",2,5,"1-493-798-9590"),(8,"Colin","Barrera",6,1,"1-661-456-3328"),(9,"Tobias","Washington",3,3,"1-209-887-5006"),(10,"Ferdinand","Boone",2,1,"1-967-876-5825");
-INSERT INTO doctor (doctor_id,first_name,last_name,department_id,specialization_id,contact_number) VALUES (11,"Kaseem","Hendricks",6,3,"1-238-830-6447"),(12,"Amal","Cervantes",10,5,"1-800-721-0707"),(13,"Hyacinth","Reyes",6,2,"1-826-344-6052"),(14,"Evangeline","Joyce",1,1,"1-165-809-6362"),(15,"Veda","Browning",6,2,"1-628-717-5652"),(16,"Raja","French",10,3,"1-348-504-1183"),(17,"Montana","Becker",9,5,"1-833-665-4335"),(18,"Vance","Melton",4,4,"1-563-691-4437"),(19,"Karyn","Stark",8,3,"1-546-904-7979"),(20,"Nathan","Lowery",8,2,"1-626-236-5030");
+INSERT INTO doctor (doctor_id,first_name,last_name,department_id,specialization_id,contact_number) VALUES 
+(1,"Garth","Walter",102,6,"1-667-914-4477"),
+(2,"Regan","Alvarado",105,15,"1-307-269-8198"),
+(3,"Hiroko","Ortega",101,4,"1-453-403-4247"),
+(4,"Bertha","Woodward",103,10,"1-495-522-6824"),
+(5,"Britanney","Maynard",100,NULL,"1-634-229-3941"),
+(6,"Kasper","Levine",104,11,"1-139-291-3738"),
+(7,"Orlando","Carr",107,23,"1-493-798-9590"),
+(8,"Colin","Barrera",102,5,"1-661-456-3328"),
+(9,"Tobias","Washington",106,18,"1-209-887-5006"),
+(10,"Ferdinand","Boone",109,29,"1-967-876-5825"),
+(11,"Kaseem","Hendricks",109,31,"1-238-830-6447"),
+(12,"Amal","Cervantes",108,26,"1-800-721-0707"),
+(13,"Hyacinth","Reyes",108,27,"1-826-344-6052"),
+(14,"Evangeline","Joyce",107,22,"1-165-809-6362"),
+(15,"Veda","Browning",109,31,"1-628-717-5652"),
+(16,"Raja","French",103,8,"1-348-504-1183"),
+(17,"Montana","Becker",105,13,"1-833-665-4335"),
+(18,"Vance","Melton",105,14,"1-563-691-4437"),
+(19,"Karyn","Stark",106,17,"1-546-904-7979"),
+(20,"Nathan","Lowery",106,19,"1-626-236-5030");
