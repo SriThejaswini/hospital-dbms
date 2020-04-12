52d47ef30839969eaa235d8ac3ed0645fde1e8c6
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-11 20:01:00 -0400

    added auto_increment to doctor.sql

--- mysql/ddl/doctor.sql
+++ mysql/ddl/doctor.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS doctor;
 
 CREATE TABLE IF NOT EXISTS doctor (
-  doctor_id int NOT NULL primary key,
+  doctor_id int NOT NULL primary key AUTO_INCREMENT,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL,
   department_id int NOT NULL references department(department_id),
   specialization_id int default NULL references specialization(specialization_id),
   contact_number varchar(100) default NULL
-);
+)AUTO_INCREMENT = 1;
