e08554de32c9ac057d055ca3244c64b6c2ff97bb
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-17 01:00:57 -0400

    Update 15_blood_donor.sql

--- mysql/ddl/15_blood_donor.sql
+++ mysql/ddl/15_blood_donor.sql
@@ -5,5 +5,7 @@ CREATE TABLE if not exists blood_donor (
   donor_name varchar(255) default NULL,
   age int ,
   gender varchar(255) default NULL,
+  blood_group ENUM('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-') NOT NULL,
+  contact_number varchar(100) default NULL,
   CHECK (age >= 18)
 ) AUTO_INCREMENT=1;
