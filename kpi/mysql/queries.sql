679a0ac907353cbd7f5e36d6886b50ac93289f75
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-17 01:08:43 -0400

    Update queries.sql

--- kpi/mysql/queries.sql
+++ kpi/mysql/queries.sql
@@ -29,3 +29,33 @@ select t.doctor_id,d.first_name,d.last_name,count(*) as no_of_patients from doct
 where d.doctor_id = t.doctor_id
 group by t.doctor_id
 order by no_of_patients desc;
+
+#Common symptoms among patients
+WITH recursive numbers AS (
+    select 1 as num
+   union all
+   select num + 1
+   from numbers
+   where num < 1000)
+select case when length(symptoms)>0 then symptoms else 'Symptoms Unknown' end as symptoms , count(1) from ( 
+  select 
+    patient_id, 
+    substring_index(
+      substring_index(replace(current_condition,' ', ''),',', num), 
+      ',', 
+      -1
+    ) as symptoms
+  from patient
+  join numbers
+    on char_length(current_condition) 
+      - char_length(replace(current_condition, ',', '')) 
+      >= num - 1
+) by_symptoms
+group by 1
+order by 2 desc;
+
+#How many donors per blood group
+select blood_group, count(*)
+from blood_donor
+group by blood_group
+order by count(*) desc;
