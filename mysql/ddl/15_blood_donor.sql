53beeb18269a7eca1a5a2f79717e385730e129cb
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-17 00:43:52 -0400

    Update 15_blood_donor.sql

--- mysql/ddl/15_blood_donor.sql
+++ mysql/ddl/15_blood_donor.sql
@@ -3,6 +3,7 @@ DROP TABLE if exists blood_donor;
 CREATE TABLE if not exists blood_donor (
   donor_ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   donor_name varchar(255) default NULL,
-  age int default NULL,
-  gender varchar(255) default NULL
+  age int ,
+  gender varchar(255) default NULL,
+  CHECK (age >= 18)
 ) AUTO_INCREMENT=1;
