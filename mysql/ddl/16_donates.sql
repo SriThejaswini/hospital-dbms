8fb4672c5c854459e8aa1aef172215943b27f97b
SriThejaswini <55600028+SriThejaswini@users.noreply.github.com>
2020-04-17 01:06:40 -0400

    Update 16_donates.sql

--- mysql/ddl/16_donates.sql
+++ mysql/ddl/16_donates.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS donates;
 
 CREATE TABLE IF NOT EXISTS donates (
-  donor_id int default NULL references donor(donor_id),
-  patient_id int default NULL references patient(patient_id),
+  donor_id int NOT NULL references donor(donor_id),
+  patient_id int NOT NULL references patient(patient_id),
+  donation_date date NOT NULL,
   PRIMARY KEY(donor_id,patient_id)
 );
